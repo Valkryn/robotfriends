e92575ea8303f2f369666c26dee7f12d18e4baf8
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2022-09-02 16:48:57 -0400

    card update

--- src/Card.js
+++ src/Card.js
@@ -3,8 +3,12 @@ import React, { Component } from "react";
 class Card extends Component {
   render() {
     return (
-      <div>
-        <h1>Card Comp</h1>
+      <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+        <img src="https://robohash.org/testing?200x200" alt="Robot" />
+        <div>
+          <h2>{this.props.name}</h2>
+          <p>{this.props.email}</p>
+        </div>
       </div>
     );
   }

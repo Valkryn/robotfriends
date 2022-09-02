93e15a54a0b9ba52fd3e5f066025fc9aa5ddbfb0
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2022-09-02 15:52:25 -0400

    init

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+// import ReactDOM from "react-dom/client";
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+export default Hello;
